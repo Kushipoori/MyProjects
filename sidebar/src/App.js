b60ed806fa711358dda065b7ae0bf61721002a51
Kushi <70482369+Kushipoori@users.noreply.github.com>
2022-01-06 15:08:40 +0530

    sidebar

---
+++ sidebar/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './App.css';
+import SideBar from './components';
+
+function App() {
+  return (
+    <div className="App">
+        <SideBar />
+    </div>
+  );
+}
+
+export default App;
